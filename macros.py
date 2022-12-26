48aedecbbaffd9b8d3759cf2c20c2997866f9563
Julius Gruber <juliusgruber@Julius-Mac.local>
2022-12-26 17:38:28 +0100

    updating the git structure

---
+++ macros.py
@@ -0,0 +1,4 @@
+gamemode = 1 # 1 for country to capital, 2 for capital to country, 3 for map of country
+highest_score_co_2_ca = 0
+highest_score_ca_2_co = 0
+highest_score_map = 0
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="geography_guessing",
+    version="0.0.2",
+    author="Julius Gruber",
+    author_email="gruber.julius@gmx.at",
+    packages=["geography_guessing"],
+    description="A package for studying geography for programmers",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gituser/test-tackage",
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=["pandas"]
+)
